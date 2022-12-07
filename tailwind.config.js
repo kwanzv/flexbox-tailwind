e02ce2bd1bfec7c6008447a046f57b178365c15a
kwanzv <kwanzeev@gmail.com>
2022-12-07 22:09:34 +0700

    Updated with dark button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
+  darkMode: "class",
   plugins: [],
 };
